ee9fece732067710523c264bec68f5d6d7d9d5ee
Thomas <thomaslindauer@mail.com>
2019-02-20 22:15:35 +0100

    FileCommencer, create new file

--- src/input.rs
+++ src/input.rs
@@ -1,25 +1,58 @@
-// @suggestion: options into a more generic 'structure'
+use std::fs::File;
+
+enum FileGrab {
+  New,
+  Existing
+}
+
+// returns created or already existing files
+pub struct FileCommencer {
+  // @todo: keep directory position state
+}
+
+impl FileCommencer {
+  fn new() -> FileCommencer {
+    FileCommencer { }
+  }
+
+  fn create(&self, name: String) -> Result<File, std::io::Error> {
+    File::create(name)
+  }
+
+  fn get(&self, name: String) -> Result<File, std::io::Error> {
+    File::open(name)
+  }
+
+  fn grab (&self, name: String, grab: FileGrab) -> Result<File, std::io::Error> {
+    // returns moved file object
+    match grab {
+      New => self.create(name),
+      Existing => self.get(name)
+    }
+  }
+}
+
+// goes into a holder to represent a selection
 pub struct SelectionPicker<'a> {
+  // a tree of choices
   options: &'a [&'a str],
 }
 
 impl<'a> SelectionPicker<'a> {
-  // @todo: this instantiation goes into a box in control of templating input data
   fn new(options: &'a [&'a str]) -> SelectionPicker<'a> {
     SelectionPicker { options }
   }
 
   // tries to match with one of option field
-  fn select(&self, input: Vec<String>) -> Option<String> {
-    // first argument is query
-    // @refactor: grabber of query from input to check if it is some.
-    let option = &input[1];
-    // find matching on option seld field
-    match self.options.iter().find(|x| **x == option) {
-      // 'unwrap' with non referencing string object
-      Some(v) => Some(String::from(*v)),
-      None => None,
-    }
+  fn select(&self, option: String) -> Option<String> {
+    // iterate try to find argument
+    self
+      .options
+      .iter()
+      // find matching on option self field
+      .find(|v| **v == option)
+      // 'unwrap' to move string object
+      .and_then(|v| Some(String::from(*v)))
   }
 }
 
@@ -29,15 +62,22 @@ mod tests {
 
   #[test]
   fn select_menu_option_create() {
-    let options = ["create", "new", "edit"];
+    let input = String::from("create");
+    let options = ["create", "edit"];
     let menu = SelectionPicker::new(&options);
-    let input = vec![String::from("texidor"), String::from("create")];
     let selected = menu.select(input);
 
-    assert!(!selected.is_none());
     match selected {
       Some(v) => assert_eq!("create", v),
       None => panic!("selected is none"),
     };
+  }
+
+  #[test]
+  fn create_new_file() {
+    let file_name = String::from("hello.txt");
+    let file_commencer = FileCommencer::new();
+    let file = file_commencer.grab(file_name, FileGrab::New);
+    assert!(file.is_ok());
   }
 }
